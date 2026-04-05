fc66da5a20ad139d6a2944911dfabb0fa041a919
h6rnyxv <diazjhordan69@gmail.com>
2026-04-05 07:35:04 -0500

    feat: add prefix/diversion/ship.js

---
+++ prefix/diversion/ship.js
@@ -0,0 +1,11 @@
+export default {
+  nombre: 'ship',
+  async ejecutar({ message }) {
+    const usuarios = [...message.mentions.users.values()];
+    if (usuarios.length < 2) return message.reply('❤️ Menciona a dos personas para shipearlas.');
+    const [user1, user2] = usuarios;
+    const porcentaje = Math.floor(Math.random() * 100) + 1;
+    const barra = '█'.repeat(Math.floor(porcentaje / 10)) + '░'.repeat(10 - Math.floor(porcentaje / 10));
+    message.channel.send(`💘 **${user1.username}** + **${user2.username}**\n\`[${barra}]\` **${porcentaje}%** de compatibilidad`);
+  },
+};
