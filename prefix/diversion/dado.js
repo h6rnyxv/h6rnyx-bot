4a2e35602655529e13182b8413e43466b00eea09
h6rnyxv <diazjhordan69@gmail.com>
2026-04-05 07:35:02 -0500

    feat: add prefix/diversion/dado.js

---
+++ prefix/diversion/dado.js
@@ -0,0 +1,9 @@
+export default {
+  nombre: 'dado',
+  async ejecutar({ message, args }) {
+    const caras = parseInt(args[0]) || 6;
+    if (caras < 2 || caras > 100) return message.reply('⚠️ El dado debe tener entre 2 y 100 caras.');
+    const numero = Math.floor(Math.random() * caras) + 1;
+    message.reply(`🎲 Has sacado un **${numero}** (dado de ${caras} caras)`);
+  },
+};
