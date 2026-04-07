a4137724cb1acd01a867471cb8da3dc02d04fed6
h6rnyxv <diazjhordan69@gmail.com>
2026-04-07 13:12:52 -0500

    migrate: sync prefix/diversion/beso.js from Layout-Bot

--- prefix/diversion/beso.js
+++ prefix/diversion/beso.js
@@ -1,5 +1,8 @@
 export default {
   nombre: 'beso',
+  descripcion: 'Besa a otro usuario.',
+  owner: false,
+
   async ejecutar({ message }) {
     const user = message.mentions.users.first();
     if (!user) return message.reply('💋 Menciona a alguien para besar.');
