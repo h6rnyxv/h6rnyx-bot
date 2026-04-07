f69636bbf4b76202627bffd1c73d60be1c06761c
h6rnyxv <diazjhordan69@gmail.com>
2026-04-07 13:12:56 -0500

    migrate: sync prefix/diversion/teamo.js from Layout-Bot

--- prefix/diversion/teamo.js
+++ prefix/diversion/teamo.js
@@ -1,5 +1,8 @@
 export default {
   nombre: 'teamo',
+  descripcion: 'Responde con cariño.',
+  owner: false,
+
   async ejecutar({ message }) {
     message.reply('💖 ¡Yo también te quiero mucho!');
   },
