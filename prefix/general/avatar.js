1c5d92283a3cdfffa450e57c023e9840788bf48d
h6rnyxv <diazjhordan69@gmail.com>
2026-06-27 04:58:11 -0500

    style: mejorar embed avatar

--- prefix/general/avatar.js
+++ prefix/general/avatar.js
@@ -1,17 +1,20 @@
-import { EmbedBuilder } from 'discord.js';
+import { embed, COLORS } from '../../utils/embed.js';
 
-export default {
-  nombre: 'avatar',
-  descripcion: 'Muestra tu avatar o el de otro usuario.',
-  owner: false,
+  export default {
+    nombre: 'avatar',
+    descripcion: 'Muestra el avatar de un usuario.',
+    uso: '[@usuario]',
+    owner: false,
 
-  async ejecutar({ message }) {
-    const miembro = message.mentions.members.first() || message.member;
-    const embed = new EmbedBuilder()
-      .setTitle(`🖼️ Avatar de ${miembro.displayName}`)
-      .setImage(miembro.displayAvatarURL({ size: 1024, dynamic: true }))
-      .setColor('Blurple');
-
-    message.channel.send({ embeds: [embed] });
-  },
-};
+    async ejecutar({ message }) {
+      const miembro = message.mentions.members.first() || message.member;
+      const url = miembro.displayAvatarURL({ size: 1024, dynamic: true });
+      message.channel.send({ embeds: [embed({
+        author: { name: miembro.displayName, iconURL: url },
+        title: '🖼️ Avatar',
+        image: url,
+        color: COLORS.primary,
+        footer: { text: `Solicitado por ${message.author.username}`, iconURL: message.author.displayAvatarURL() },
+      })] });
+    },
+  };
